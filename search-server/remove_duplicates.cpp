2949658e8507bfad3839595d1f64591d18607a82
Sergey <cftasd@yandex.ru>
2023-04-05 13:47:21 +0300

    sprint 6 final

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -9,43 +9,21 @@ using namespace std::string_literals;
 void RemoveDuplicates(SearchServer& search_server) {
 	std::set<int> id_to_delete;
 
-	std::map< std::set< std::string>, std::set<int>> wordsset_idset;
+	std::set<std::set<std::string>> words_set;
 
-	std::map< int, std::set< std::string>> id_wordsset;
-
-
-	for (int doc_id : search_server) { //O(N)
+	for (int doc_id : search_server) {
 		std::set< std::string> words;
 
-		for (const auto& [word, nouseddouble] : search_server.GetWordFrequencies(doc_id)) {	//O(w)
-			words.insert(word);	//log(w)
+		for (const auto& [word, nouseddouble] : search_server.GetWordFrequencies(doc_id)) {
+			words.insert(word);
 		}
-
-		wordsset_idset[words].insert(doc_id); //Log(N)
-		id_wordsset[doc_id] = words; //Log(N)
-	}//O(N (Log (w) + Log(N))
-
-	//сама реализация
-	for (int doc_id : search_server) {	//O(N)
-		if (id_to_delete.count(doc_id)) {
-			continue;
-		}
-		else {
-			const std::set<int>& cheked_id = wordsset_idset[id_wordsset.at(doc_id)];
-			if (cheked_id.size() != 1) {  //O(1)
-				id_to_delete.insert(next(cheked_id.begin(), 1), cheked_id.end()); //В наихудшем случае с учётом цикла - O(N);
-			}
+		if (!words_set.emplace(words).second) {
+			id_to_delete.insert(doc_id);
 		}
 	}
-
 
 	for (int id : id_to_delete) {
 		std::cout << "Found duplicate document id "s << id << std::endl;
 		search_server.RemoveDocument(id);
 	}
-	//
-	//std::cout << "After duplicates removed: "s << search_server.GetDocumentCount() << std::endl;
 }
-
-
-
